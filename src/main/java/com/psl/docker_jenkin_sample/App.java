639fb8fcaa0216a34b33d5afbef04852c3505b4c
VaishnaviNayak <vaishnavinayak1@gmail.com>
2020-10-12 11:25:38 +0530

    F1:First Commit

---
+++ src/main/java/com/psl/docker_jenkin_sample/App.java
@@ -0,0 +1,13 @@
+package com.psl.docker_jenkin_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
